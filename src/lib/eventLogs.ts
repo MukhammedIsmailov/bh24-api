f6bd5b8825060f5c546678c44fd5f454d9fe8216
Mukhammed Ismailov <mukhammed.ismailov@pluto.tv>
2019-12-02 17:50:53 +0300

    add statistics for counts and fixes for plot

--- src/lib/eventLogs.ts
+++ src/lib/eventLogs.ts
@@ -4,4 +4,5 @@ export enum EventLogs {
     courseFinished = 'CF',
     feedbackButtonClick = 'FB',
     newPartner = 'NP',
+    paidAffiliate = 'PA',
 }
--- src/modules/statistics/statistics.controller.ts
+++ src/modules/statistics/statistics.controller.ts
@@ -18,13 +18,72 @@ export class StatisticsController {
         const endDate = endDateTime.format(psqlDateFormat);
 
         try {
+            const eventTypes: string[] = Object.keys(EventLogs).map(key => { return EventLogs[key] });
+
             const eventRepository = getManager().getRepository(EventEntity);
 
-            const dataFromDb = await eventRepository.query(`SELECT to_char(date_trunc('day', created_date), 
-                'DD MON YY') AS date, count(1), event_log FROM event WHERE created_date > '${startDate}' 
+            const countsData = await eventRepository.query(`SELECT count(id), event_log FROM event
+                WHERE created_date > '${startDate}' AND created_date < '${endDate}' GROUP BY event_log;`);
+
+            const totalCount = await eventRepository.query('SELECT count(id) FROM event;');
+
+            const countsForPaymentEfficiency = await eventRepository.query(`SELECT count(id), event_log FROM event 
+                WHERE event_log = '${EventLogs.landingVisit}' OR event_log = '${EventLogs.paidAffiliate}' GROUP BY event_log;`);
+
+            const countsForCourseEfficiency = await eventRepository.query(`SELECT count(id), event_log FROM event 
+                WHERE event_log = '${EventLogs.courseSubscription}' OR event_log = '${EventLogs.courseFinished}' GROUP BY event_log;`);
+
+            const dataForPlot = await eventRepository.query(`SELECT to_char(date_trunc('day', created_date),
+                'DD MON YY') AS date, count(1), event_log FROM event WHERE created_date > '${startDate}'
                 AND created_date < '${endDate}' GROUP BY 1, event_log;`);
 
-            console.log(dataFromDb)
+            const countOfCourseFinishedRAW = countsForCourseEfficiency.find(item => { return item['event_log'] === EventLogs.courseFinished });
+            const countOfCourseSubscriptionRAW = countsForCourseEfficiency.find(item => { return item['event_log'] === EventLogs.courseSubscription });
+
+            const countOfTransitionsRAW = countsForPaymentEfficiency.find(item => { return item['event_log'] === EventLogs.landingVisit });
+            const countOfPaymentsRAW = countsForPaymentEfficiency.find(item => { return item['event_log'] === EventLogs.paidAffiliate });
+
+            const countOfTransitions = !!countOfTransitionsRAW ? parseInt(countOfTransitionsRAW['count']) : 0;
+            const countOfPayments = !!countOfPaymentsRAW ? parseInt(countOfPaymentsRAW['count']) : 0;
+
+            const countOfCourseFinished = !!countOfCourseFinishedRAW ? parseInt(countOfCourseFinishedRAW['count']) : 0;
+            const countOfCourseSubscription = !!countOfCourseSubscriptionRAW ? parseInt(countOfCourseSubscriptionRAW['count']) : 0;
+
+            const paymentEfficiency = !!countOfTransitions && !!countOfPayments ?  countOfPayments / countOfTransitions * 100 : 0;
+            const courseEfficiency = !!countOfCourseFinished && !!countOfCourseSubscription ?  countOfCourseFinished / countOfCourseSubscription * 100 : 0;
+
+            const counts = [];
+            const total = parseInt(totalCount[0]['count']);
+            const plotData = [];
+
+            for (const type of eventTypes) {
+                plotData.push({ [type ]: []});
+                counts.push({ [type]: 0 });
+                for (const item of countsData) {
+                    if (type === item['event_log']) {
+                        counts[counts.length - 1][type] = parseInt(item['count']);
+                        break;
+                    }
+                }
+
+                for (const item of dataForPlot) {
+                    if (type === item['event_log']) {
+                        plotData[plotData.length - 1][type].push({
+                            date: item['date'],
+                            count: item['count'],
+                            timestamp: parseInt(moment.utc(item['date'], 'DD MMM YY').format('X')),
+                        });
+                    }
+                }
+            }
+
+            ctx.body = {
+                counts,
+                total,
+                paymentEfficiency,
+                courseEfficiency,
+                plotData
+            };
 
             ctx.status = 200;
 
