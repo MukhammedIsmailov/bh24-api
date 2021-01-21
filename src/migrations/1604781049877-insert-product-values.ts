29428ac21e76012bdb9f9ea343a9d471b8eb2a95
Nazar Bashinskiy <nazar_bashinskiy@mail.ru>
2021-01-21 16:51:14 +0300

    fix

--- src/migrations/1604781049877-insert-product-values.ts
+++ src/migrations/1604781049877-insert-product-values.ts
@@ -4,26 +4,7 @@ import { ProductEntity } from '../modules/product/product.entity';
 export class insertProductValues1604781049877 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const connection = getConnection();
-            await connection.createQueryBuilder().insert().into(ProductEntity).values([
-            {
-                name: 'Базовый',
-                price: 36,
-                duration: 90
-            },
-
-            {
-                name: 'Оптимальный',
-                price: 72,
-                duration: 180
-            },
-
-            {
-                name: 'Лидерский',
-                price: 144,
-                duration: 360
-            }
-        ]).execute()
+        await queryRunner.query(`INSERT INTO "product"("name", "price", "duration") VALUES ('Базовый', 3600, 90), ('Оптимальный', 7200, 180), ('Лидерский', 14400, 180)  RETURNING "id"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
