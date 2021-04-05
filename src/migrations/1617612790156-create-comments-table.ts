f10e341b61230aac968ad13fe6bc352aa66d3616
Nazar Bashinskiy <nazar_bashinskiy@mail.ru>
2021-04-05 12:14:38 +0300

    created comments table

---
+++ src/migrations/1617612790156-create-comments-table.ts
@@ -0,0 +1,52 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey, TableColumn} from 'typeorm';
+
+export class createCommentsTable1617612790156 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'comments',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'createdDate',
+                    type: 'timestamp'
+                }
+            ]
+        }));
+
+        await queryRunner.addColumn('comments', new TableColumn({
+            name: 'user_id',
+            type: 'int'
+        }));
+
+        await queryRunner.createForeignKey('comments', new TableForeignKey({
+            columnNames: ['user_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'user',
+            onDelete: 'CASCADE'
+        }));
+
+        await queryRunner.addColumn('comments', new TableColumn({
+            name: 'lesson_id',
+            type: 'int'
+        }));
+
+        await queryRunner.createForeignKey('comments', new TableForeignKey({
+            columnNames: ['lesson_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'lesson',
+            onDelete: 'CASCADE'
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('comments');
+    }
+
+}
