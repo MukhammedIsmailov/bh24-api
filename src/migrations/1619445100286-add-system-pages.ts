0861ae719ad6d236cdfaa179c59126f69d8862a1
Nazar Bashinskiy <nazar_bashinskiy@mail.ru>
2021-04-26 17:17:51 +0300

    added system pages to db

---
+++ src/migrations/1619445100286-add-system-pages.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class addSystemPages1619445100286 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const query = `INSERT INTO page (is_system, name, verbose_name, content) VALUES 
+                       (true, 'main', 'Главная',  '{}'),
+                       (true, 'education', 'Обучение', '{}'),
+                       (true, 'calendar', 'Календарь', '{}'),
+                       (true, 'promo', 'Промо', '{}')`;
+        await queryRunner.query(query);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DELETE FROM page WHERE name IN ('main', 'education', 'calendar', 'promo')`);
+    }
+
+}
